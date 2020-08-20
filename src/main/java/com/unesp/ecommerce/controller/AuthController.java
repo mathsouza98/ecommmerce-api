aa20b567a9939961bb68d534e03608f8949f3f2f
Matheus Souza <matheus.98.2013@gmail.com>
2020-08-20 08:41:16 -0300

    Implement security on admin/moderator signup

--- src/main/java/com/unesp/ecommerce/controller/AuthController.java
+++ src/main/java/com/unesp/ecommerce/controller/AuthController.java
@@ -1,27 +1,28 @@
 package com.unesp.ecommerce.controller;
 
+import com.unesp.ecommerce.model.ERole;
+import com.unesp.ecommerce.model.Role;
 import com.unesp.ecommerce.payload.request.LoginRequest;
 import com.unesp.ecommerce.payload.request.SignupRequest;
 import com.unesp.ecommerce.payload.response.JwtResponse;
-import com.unesp.ecommerce.payload.response.MessageResponse;
-import com.unesp.ecommerce.repository.LegalUserRepository;
-import com.unesp.ecommerce.repository.PhysicalUserRepository;
-import com.unesp.ecommerce.repository.UserRepository;
+import com.unesp.ecommerce.repository.RoleRepository;
 import com.unesp.ecommerce.security.jwt.JwtUtils;
 import com.unesp.ecommerce.security.services.UserDetailsImpl;
 import com.unesp.ecommerce.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -32,19 +33,10 @@ public class AuthController {
     AuthenticationManager authenticationManager;
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    PhysicalUserRepository physicalUserRepository;
-
-    @Autowired
-    LegalUserRepository legalUserRepository;
+    RoleRepository roleRepository;
 
     @Autowired
     UserService userService;
-
-    @Autowired
-    PasswordEncoder encoder;
 
     @Autowired
     JwtUtils jwtUtils;
@@ -72,33 +64,21 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: Username is already taken!"));
-        }
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        Set<Role> roles = new HashSet<>();
 
-        if ( signUpRequest.getCpf() != null ) {
-            if(physicalUserRepository.existsByCpf(signUpRequest.getCpf())) {
-                return ResponseEntity
-                        .badRequest()
-                        .body(new MessageResponse("Error: Cpf already taken!"));
-            } else {
-                return userService.signupPhysicalUser(signUpRequest);
-            }
-        }
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(userRole);
 
-        if ( signUpRequest.getCnpj() != null ) {
-            if(legalUserRepository.existsByCnpj(signUpRequest.getCnpj())) {
-                return ResponseEntity
-                        .badRequest()
-                        .body(new MessageResponse("Error: Cnpj already taken!"));
-            } else {
-                return userService.signupLegalUser(signUpRequest);
-            }
-        }
+        return userService.signupUser(signupRequest, roles);
+    }
 
-        return ResponseEntity.badRequest().body(new MessageResponse("Error trying to register user!"));
+    @PostMapping("/admin/signup")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> registerRoleBasedUser(@Valid @RequestBody SignupRequest signupRequest) {
+        Set<Role> roles = userService.getSetOfRoleBySignupRequest(signupRequest);
+
+        return userService.signupUser(signupRequest, roles);
     }
 }
