8185042833203c1ff89dae9955367e3641221729
Matheus Souza <matheus.98.2013@gmail.com>
2020-09-07 21:29:01 -0300

    feat: prefix dynamization

--- src/main/java/com/unesp/ecommerce/security/jwt/JwtUtils.java
+++ src/main/java/com/unesp/ecommerce/security/jwt/JwtUtils.java
@@ -20,7 +20,7 @@ public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${ecommerce.globalVariables.tokenPrefix}")
-    String tokenPrefix;
+    private String tokenPrefix;
 
     @Value("${ecommerce.globalVariables.jwtExpirationMs}")
     private int jwtExpirationMs;
