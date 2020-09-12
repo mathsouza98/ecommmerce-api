ae6d108ac3ff9fc41671b94cf437166746d7ceac
Matheus Souza <matheus.98.2013@gmail.com>
2020-09-12 09:39:51 -0300

    feat: add get cart controller

--- src/main/java/com/unesp/ecommerce/controller/CartController.java
+++ src/main/java/com/unesp/ecommerce/controller/CartController.java
@@ -1,8 +1,8 @@
 package com.unesp.ecommerce.controller;
 
+import com.unesp.ecommerce.model.Cart;
 import com.unesp.ecommerce.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,7 +16,12 @@ public class CartController {
     @PostMapping("/cart/{productId}")
     //@PostAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
     public String buyProduct(@PathVariable String productId, @RequestHeader(required = false, value = "CartId") String cartId, @RequestHeader(value = "Authorization") String authorization) {
-
         return cartService.addProductOnCart(productId, cartId, authorization);
+    }
+
+    @GetMapping("/cart")
+    //@PostAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') hasRole('USER')")
+    public Cart listCartByAuthToken(@RequestHeader(value = "Authorization") String authorization) {
+        return cartService.getCart(authorization)
     }
 }
