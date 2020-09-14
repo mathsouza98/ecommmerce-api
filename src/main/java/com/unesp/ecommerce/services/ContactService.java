49a5474e6ef7c82cd5801b7111a5f548a875dd7b
Matheus Souza <matheus.98.2013@gmail.com>
2020-09-14 19:49:12 -0300

    feat: Implement user contact methods

--- src/main/java/com/unesp/ecommerce/services/ContactService.java
+++ src/main/java/com/unesp/ecommerce/services/ContactService.java
@@ -1,25 +1,40 @@
 package com.unesp.ecommerce.services;
 
-import com.unesp.ecommerce.model.Cart;
-import com.unesp.ecommerce.model.Product;
+import com.unesp.ecommerce.model.Contact;
 import com.unesp.ecommerce.model.User;
-import com.unesp.ecommerce.repository.CartRepository;
+import com.unesp.ecommerce.repository.UserRepository;
 import com.unesp.ecommerce.security.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ContactService {
 
     @Autowired
-    private ContactRepository contactRepository;
-
-    @Autowired
     JwtUtils jwtUtils;
 
-    
+    @Autowired
+    UserRepository userRepository;
+
+    public Contact getUserContact(String authorization) {
+        User user = jwtUtils.getUserByAuthorization(authorization);
+
+        return user.getContact();
+    }
+
+    public void deleteUserContact(String authorization) {
+        User user = jwtUtils.getUserByAuthorization(authorization);
+
+        user.setContact(null);
+        userRepository.save(user);
+    }
+
+    public Contact updateUserContact(String authorization, Contact contact) {
+        User user = jwtUtils.getUserByAuthorization(authorization);
+
+        user.setContact(contact);
+        userRepository.save(user);
+
+        return user.getContact();
+    }
 }
