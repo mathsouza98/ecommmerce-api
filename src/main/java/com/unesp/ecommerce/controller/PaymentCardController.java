5474ec3cf0374795811975104cd265d117f69b90
matsouza <matsouza@uolinc.com>
2020-09-28 15:29:19 -0300

    fix: pre authorize filter

--- src/main/java/com/unesp/ecommerce/controller/PaymentCardController.java
+++ src/main/java/com/unesp/ecommerce/controller/PaymentCardController.java
@@ -5,6 +5,7 @@ import com.unesp.ecommerce.services.PaymentCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,25 +19,25 @@ public class PaymentCardController {
     PaymentCardService paymentCardService;
 
     @GetMapping("/payment-card")
-    @PostAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
     public List<PaymentCard> listPaymentCardByAuthToken(@RequestHeader(value = "Authorization") String authorization) {
         return paymentCardService.listPaymentCard(authorization);
     }
 
     @PostMapping("/payment-card")
-    @PostAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity savePaymentCard(@RequestBody PaymentCard paymentCard, @RequestHeader(value = "Authorization") String authorization) {
         return paymentCardService.savePaymentCard(paymentCard, authorization);
     }
 
     @PutMapping("/payment-card/{paymentCardId}")
-    @PostAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity updatePaymentCard(@RequestBody PaymentCard paymentCard, @PathVariable(value = "paymentCardId") String paymentCardId, @RequestHeader(value = "Authorization") String authorization) {
         return paymentCardService.updatePaymentCard(paymentCard, paymentCardId, authorization);
     }
 
     @DeleteMapping("/payment-card/{paymentCardId}")
-    @PostAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity deletePaymentCard(@PathVariable(value = "paymentCardId") String paymentCardId, @RequestHeader(value = "Authorization") String authorization) {
         return paymentCardService.deletePaymentCard(paymentCardId, authorization);
     }
